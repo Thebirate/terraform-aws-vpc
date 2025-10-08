9e3f83883f8f49d4edcb93774145fbc8fde4e947
Thebirate <60749357+Thebirate@users.noreply.github.com>
2025-10-08 00:04:45 -0700

    fixed the outputs22 error

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
 output "vpc_id" {
   description = "The ID of the VPC."
-  value       = module_vpc.main.id
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
   description = "A list of public subnet IDs."
-  value       = module_subnet.public[*].id
+  value       = aws_subnet.public.*.id
 }
 
 output "private_subnet_ids" {
   description = "A list of private subnet IDs."
-  value       = module_subnet.private[*].id
+  value       = aws_subnet.private.*.id
+}
+
+output "security_group_id" {
+  description = "The ID of the security group for EC2 instances."
+  value       = aws_security_group.ec2_sg.id
+}
+
+output "internet_gateway_id" {
+  description = "The ID of the created Internet Gateway."
+  value       = aws_internet_gateway.main.id
 }
